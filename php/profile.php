4858f4bb44763621518d3897762d285513abb31b
JAGATHRATCHAKAN S <82392148+Jagathratchakan@users.noreply.github.com>
2023-11-26 11:08:20 +0530

    Update profile.php

--- php/profile.php
+++ php/profile.php
@@ -1,13 +1,25 @@
 <?php
 
+$email = $_POST["Email"];
 $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
-$query = new MongoDB\Driver\Query(array('Email' => 'jagath@gmail.com'));
+$query = new MongoDB\Driver\Query(array('Email' => $email));
 
 
 $cursor = $manager->executeQuery('Profile.profile', $query);
 
 
 $profileData = $cursor->toArray();
+
+if (empty($profileData)) {
+    $bulk = new MongoDB\Driver\BulkWrite;
+    
+    $bulk->insert(array(
+      'Email' => $email
+    ));
+  
+    $manager->executeBulkWrite('Profile.profile', $bulk);
+    echo json_encode(array('Phone' =>'Enter Number','Email' =>$email,'Age'=>'Enter Age','Name'=>'Enter Name',"id"=>'Enter id'));
+}else{
 
 header("Content-type: application/json");
 
@@ -20,5 +32,5 @@ echo json_encode(array('Phone' =>$pho,'Email' =>$em,'Age'=>$age,'Name'=>$name,"i
 //echo json_encode(array('Email' =>$em));
 //echo json_encode(array('Age' =>$age));
 //echo json_encode(array('Age' =>$profileData[0]->Age));
-
+}
 ?>
